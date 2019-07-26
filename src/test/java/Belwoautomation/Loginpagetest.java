7dd684059a5100ee49cf496e2493af1176135b1e
Workstation-6 <Workstation-6@Workstation-06>
2019-07-27 00:37:49 +0530

    1

--- src/test/java/Belwoautomation/Loginpagetest.java
+++ src/test/java/Belwoautomation/Loginpagetest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.belwoautomation.qa.base.Testbase;
+import com.belwoautomation.qa.pages.Homepage;
 import com.belwoautomation.qa.pages.Loginpage;
 
 import junit.framework.Assert;
@@ -11,28 +12,27 @@ import junit.framework.Assert;
 public class Loginpagetest extends Testbase {
 
 	Loginpage loginpage;
-	public Loginpagetest()
-	{
+	Homepage homepage;
+
+	public Loginpagetest() {
 		super();
 	}
-	
-	@BeforeMethod	
-		public void setUp()
-		{
-		System.out.println("Test 2");
+
+	@BeforeMethod
+	public void setUp() {
+
 		initialization();
-		
-		loginpage=new Loginpage();
-		}
-	@Test
-	public void loginPageTitleTest()
-	{
-		
-	}
-	@Test
-	public void logintest()
-	{
-		loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
-	}
+
+		loginpage = new Loginpage();
 	}
 
+	@Test
+	public void loginPageTitleTest() {
+
+	}
+
+	@Test
+	public void logintest() {
+		homepage = loginpage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+}
